17579a5d16670c2157a45be987b2bb9e4cd99933
Samuel Ellicott <nebk2010@gmail.com>
2016-06-09 15:10:56 -0500

    newly generated code

--- Src/main.c
+++ Src/main.c
@@ -32,7 +32,6 @@
   */
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f0xx_hal.h"
-#include "usb_device.h"
 
 /* USER CODE BEGIN Includes */
 #include "usbd_cdc_if.h"
@@ -44,6 +43,11 @@
 #define IO3_ADC ADC_CHSELR_CHSEL9
 /* USER CODE END Includes */
 
+/* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef hadc;
+
+CAN_HandleTypeDef hcan;
+
 /* USER CODE BEGIN PV */
 /* Private variables ---------------------------------------------------------*/
 
@@ -53,6 +57,7 @@
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_ADC_Init(void);
+static void MX_CAN_Init(void);
 
 /* USER CODE BEGIN PFP */
 /* Private function prototypes -----------------------------------------------*/
@@ -65,10 +70,13 @@ static void MX_ADC_Init(void);
 
 int main(void)
 {
-  uint8_t buff[] = "Hello World\n\r";
-  uint16_t buff_len = 14;
-  uint16_t data;
- 
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration----------------------------------------------------------*/
+
   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
 
@@ -78,48 +86,19 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_ADC_Init();
-  MX_USB_DEVICE_Init();
+  MX_CAN_Init();
 
-  //select IO1 for ADC conversion
-  ADC1->CHSELR = IO3_ADC;
-  
-  HAL_Delay(300);
-  
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
   while (1)
   {
-     //start ADC conversion
-     ADC1->CR |= ADC_CR_ADSTART;
-     
-     LED1_GPIO_Port->BSRR |= LED1_Pin;
-     LED2_GPIO_Port->BSRR |= LED2_Pin << 16;
-     HAL_Delay(100);
-     
-     //wait for conversion to finish
-     while(ADC1->CR & ADC_CR_ADSTART) {
-        HAL_Delay(1);
-     }
-     data = ADC1->DR;
+  /* USER CODE END WHILE */
 
-     itoa(data, buff, 10);
-     strcat(buff, "\n\r");
-     CDC_Transmit_FS(buff, strlen(buff));
-     
-     //start ADC conversion
-     ADC1->CR |= ADC_CR_ADSTART;
-     
-     LED1_GPIO_Port->BSRR |= LED1_Pin << 16;
-     LED2_GPIO_Port->BSRR |= LED2_Pin;
-     HAL_Delay(100);
-
-     //wait for conversion to finish
-     while(ADC1->CR & ADC_CR_ADSTART) {
-        HAL_Delay(1);
-     }
-     data = ADC1->DR;
-     
-     itoa(data, buff, 10);
-     strcat(buff, "\n\r");
-     CDC_Transmit_FS(buff, strlen(buff));
+  /* USER CODE BEGIN 3 */
 
   }
   /* USER CODE END 3 */
@@ -128,13 +107,11 @@ int main(void)
 
 /** System Clock Configuration
 */
-
 void SystemClock_Config(void)
 {
 
   RCC_OscInitTypeDef RCC_OscInitStruct;
   RCC_ClkInitTypeDef RCC_ClkInitStruct;
-  RCC_PeriphCLKInitTypeDef PeriphClkInit;
 
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI14|RCC_OSCILLATORTYPE_HSI48;
   RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
@@ -150,10 +127,6 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
   HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1);
 
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB;
-  PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_HSI48;
-  HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit);
-
   HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
 
   HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
@@ -163,24 +136,67 @@ void SystemClock_Config(void)
 }
 
 /* ADC init function */
-
 void MX_ADC_Init(void)
 {
-    //configure ADC clock
-    RCC->APB2ENR |= RCC_APB2ENR_ADC1EN; //enable ADC clock
-    RCC->CR2 |= RCC_CR2_HSI14ON; //enable sample clock
-    while ((RCC->CR2 & RCC_CR2_HSI14RDY) == 0) { //wait for clock to start up
-        HAL_Delay(1);
-    }
 
-    //configure ADC
-    ADC1->CR |= ADC_CR_ADEN; //enable ADC
-    while ((ADC1->ISR & ADC_ISR_ADRDY) == 0){ //wait until ready
-        HAL_Delay(1);
-    }
-    ADC1->CFGR2 &= (~ADC_CFGR2_CKMODE); //set ADC clock mode
+  ADC_ChannelConfTypeDef sConfig;
+
+    /**Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion) 
+    */
+  hadc.Instance = ADC1;
+  hadc.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV1;
+  hadc.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc.Init.ScanConvMode = ADC_SCAN_DIRECTION_FORWARD;
+  hadc.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  hadc.Init.LowPowerAutoWait = DISABLE;
+  hadc.Init.LowPowerAutoPowerOff = DISABLE;
+  hadc.Init.ContinuousConvMode = DISABLE;
+  hadc.Init.DiscontinuousConvMode = DISABLE;
+  hadc.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc.Init.DMAContinuousRequests = DISABLE;
+  hadc.Init.Overrun = ADC_OVR_DATA_PRESERVED;
+  HAL_ADC_Init(&hadc);
+
+    /**Configure for the selected ADC regular channel to be converted. 
+    */
+  sConfig.Channel = ADC_CHANNEL_6;
+  sConfig.Rank = ADC_RANK_CHANNEL_NUMBER;
+  sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
+  HAL_ADC_ConfigChannel(&hadc, &sConfig);
+
+    /**Configure for the selected ADC regular channel to be converted. 
+    */
+  sConfig.Channel = ADC_CHANNEL_7;
+  HAL_ADC_ConfigChannel(&hadc, &sConfig);
+
+    /**Configure for the selected ADC regular channel to be converted. 
+    */
+  sConfig.Channel = ADC_CHANNEL_9;
+  HAL_ADC_ConfigChannel(&hadc, &sConfig);
 
 }
+
+/* CAN init function */
+void MX_CAN_Init(void)
+{
+
+  hcan.Instance = CAN;
+  hcan.Init.Prescaler = 16;
+  hcan.Init.Mode = CAN_MODE_NORMAL;
+  hcan.Init.SJW = CAN_SJW_1TQ;
+  hcan.Init.BS1 = CAN_BS1_1TQ;
+  hcan.Init.BS2 = CAN_BS2_1TQ;
+  hcan.Init.TTCM = DISABLE;
+  hcan.Init.ABOM = DISABLE;
+  hcan.Init.AWUM = DISABLE;
+  hcan.Init.NART = DISABLE;
+  hcan.Init.RFLM = DISABLE;
+  hcan.Init.TXFP = DISABLE;
+  HAL_CAN_Init(&hcan);
+
+}
+
 /** Configure pins as 
         * Analog 
         * Input 
@@ -188,23 +204,57 @@ void MX_ADC_Init(void)
         * EVENT_OUT
         * EXTI
 */
-void MX_GPIO_Init(void) {
+void MX_GPIO_Init(void)
+{
 
-    //GPIO Clock enable
-    RCC->AHBENR |= RCC_AHBENR_GPIOAEN | RCC_AHBENR_GPIOBEN;
+  GPIO_InitTypeDef GPIO_InitStruct;
 
-    //set initial output to low
-    GPIOA->BSRR |= (LED1_Pin | LED2_Pin) << 16;
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
-    //configure the pin settings LED1 (PA4) and LED2 (PA5)
-    GPIOA->MODER |= GPIO_MODER_MODER4_0 | GPIO_MODER_MODER5_0; //set to output modes
-    //all other settings are okay
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, LED2_Pin|LED1_Pin, GPIO_PIN_RESET);
 
-    //setup PA6 (IO1), PA7 (IO2), and PB1(IO3) as analog inputs
-    GPIOA->MODER |= GPIO_MODER_MODER6_0 | GPIO_MODER_MODER6_1 //PA6
-               |  GPIO_MODER_MODER7_0 | GPIO_MODER_MODER6_1;//PA7
-
-    GPIOB->MODER |= GPIO_MODER_MODER1_0 | GPIO_MODER_MODER1_1;//PB1
-  
+  /*Configure GPIO pins : LED2_Pin LED1_Pin */
+  GPIO_InitStruct.Pin = LED2_Pin|LED1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
 }
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+#ifdef USE_FULL_ASSERT
+
+/**
+   * @brief Reports the name of the source file and the source line number
+   * where the assert_param error has occurred.
+   * @param file: pointer to the source file name
+   * @param line: assert_param error line source number
+   * @retval None
+   */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+    ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+
+}
+
+#endif
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+*/ 
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
