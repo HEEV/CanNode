4c1a274c9f906cde16b8e51df10ed9a5a0d60d2d
Sam Ellicott <sellicott@cedarville.edu>
2017-09-09 19:11:25 -0400

    platform specific include files

---
+++ CanNode/platform.h
@@ -0,0 +1,8 @@
+#ifndef _PLATFORM_CAN_H_
+#define _PLATFORM_CAN_H_
+#include <stm32f0xx.h>
+#include <mxconstants.h>
+#include <stm32f0xx_hal.h>
+#include <stm32f0xx_hal_can.h>
+
+#endif //_PLATFORM_CAN_H_
